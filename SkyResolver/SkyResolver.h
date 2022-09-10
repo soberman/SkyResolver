daa0025d59e54940624881ef65f2d92a41cc5039
Yarik Arsenkin <yarik@arsenkin.com>
2022-09-10 21:53:41 +0200

    Initial Commit

---
+++ SkyResolver/SkyResolver.h
@@ -0,0 +1,18 @@
+//
+//  SkyResolver.h
+//  SkyResolver
+//
+//  Created by Yaroslav Arsenkin on 10.09.22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SkyResolver.
+FOUNDATION_EXPORT double SkyResolverVersionNumber;
+
+//! Project version string for SkyResolver.
+FOUNDATION_EXPORT const unsigned char SkyResolverVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SkyResolver/PublicHeader.h>
+
+
