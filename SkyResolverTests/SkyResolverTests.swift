17649b7e1b29a90610a7c75e95f009d05a8d1bdb
Yarik Arsenkin <yarik@arsenkin.com>
2022-09-11 02:15:39 +0200

    Add test for a complex dependency resolve and registration.

--- SkyResolverTests/SkyResolverTests.swift
+++ SkyResolverTests/SkyResolverTests.swift
@@ -1,18 +1,6 @@
 import XCTest
 @testable import SkyResolver
 
-protocol TestSubject: AnyObject {}
-class A: TestSubject {}
-class C: TestSubject {
-    let sample = "sample"
-}
-class B {
-    let a: TestSubject
-    init(a: TestSubject) {
-        self.a = a
-    }
-}
-
 class SkyResolverTests: XCTestCase {
 
     override func tearDown() {
@@ -63,4 +51,58 @@ class SkyResolverTests: XCTestCase {
         XCTAssertTrue(true, "We did not encounter fatal exception and resolved object successfully")
     }
 
+    func testRegisterAndResolveOfComplexDependantObjects() {
+        SkyResolver.shared.register { Worker() as Workerhaving }
+        SkyResolver.shared.register { Manager(worker: try! SkyResolver.shared.resolve()) as ManagerHaving }
+        SkyResolver.shared.register { Director(manager: try! SkyResolver.shared.resolve()) as DirectorHaving }
+
+        let _: DirectorHaving = try! SkyResolver.shared.resolve()
+        XCTAssertTrue(true, "We did not encounter fatal exception and resolved object successfully")
+    }
+
+}
+
+protocol TestSubject: AnyObject {}
+
+class A: TestSubject {}
+
+class C: TestSubject {
+    let sample = "sample"
+}
+
+class B {
+    let a: TestSubject
+    init(a: TestSubject) {
+        self.a = a
+    }
+}
+
+class D {
+    let e: E
+    init(e: E) {
+        self.e = e
+    }
+}
+class E {
+    let d: D
+    init(d: D) {
+        self.d = d
+    }
+}
+
+protocol Workerhaving {}
+class Worker: Workerhaving {}
+protocol ManagerHaving {}
+class Manager: ManagerHaving {
+    let worker: Workerhaving
+    init(worker: Workerhaving) {
+        self.worker = worker
+    }
+}
+protocol DirectorHaving {}
+class Director: DirectorHaving {
+    let manager: ManagerHaving
+    init(manager: ManagerHaving) {
+        self.manager = manager
+    }
 }
